684328e8e60a828177fa09cfd4009de4dee6435f
Killcat <catalin.cereanu@cs.upt.ro>
2023-08-01 01:02:02 +0300

    Modified setup script

--- setup.sh
+++ setup.sh
@@ -3,16 +3,8 @@
 set -e
 set -u
 
-ACTIVE_SHELL_NAME=`echo $(ps -p $$ | tr '\n' ' ' | tr '\t' ' ') | awk '{n=split($0,a," "); print a[n]}'`
-
-[ ! -e ~/.${ACTIVE_SHELL_NAME}rc ] && echo "No common source file found for the active shell\nAbort!" && return 1
-
-cat ./changes >> ~/.${ACTIVE_SHELL_NAME}rc
-
 mkdir -p ~/.sources
 
 cp .sources/* ~/.sources/
 
-source ~/.${ACTIVE_SHELL_NAME}rc && echo "Custom shell functions successfully installed!"
-
-unset ACTIVE_SHELL_NAME
+echo -e "\nRun:\n\ncat ./changes >> <rc_file>; source <rc_file>\n\nWhere <rc_file> is your current rc file (~/.zshrc, ~/.bashrc, etc)\n\n"
