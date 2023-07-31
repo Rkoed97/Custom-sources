589e6f6b4623e147630a4f1390bc896a0144e79a
Killcat <catalin.cereanu@cs.upt.ro>
2023-08-01 00:11:47 +0300

    added reset shell command

--- .sources/activate_venv.sh
+++ .sources/activate_venv.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+: 'Copyright Rkoed97
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'
+
 activate_venv() {
 	# Check if the parameter is specified
 	[ ! $1 ] && echo "No venv specified for activation!" && return 1
@@ -8,7 +23,7 @@ activate_venv() {
 	[ ! -d "$1" ] && echo "Specified venv does not exist here!" && return 2
 
 	# Check if the folder is a venv
-    # Limited check, lazy for more
+	# Limited check, lazy for more
 	[[ ! -d "$1/bin" || ! -d "$1/lib" ]] && echo "Specified folder is not a venv!" && return 3
 
 	# Check if we can activate the venv
--- .sources/reset_shell.sh
+++ .sources/reset_shell.sh
@@ -15,9 +15,12 @@
    limitations under the License.
 '
 
-create_venv() {
-	# Check if the parameter is specified
-	[ ! $1 ] && echo "No venv specified for creation!" && return 1
+reset_shell() {
+	# Check and retain the active shell name
+	ACTIVE_SHELL_NAME=$(echo $0 | awk '{n=split($0,a,"/"); print a[n]}')
 
-	virtualenv "$1" && echo "Environment successfully created!" && return 0
+	# Searching for common .*rc files, based on common naming convetions
+	[ ! -e ~/.${ACTIVE_SHELL_NAME}rc ] && echo "No common source for the active shell" && return 1
+
+	source ~/.${ACTIVE_SHELL_NAME}rc && echo "Shell successfully reset!"
 }
